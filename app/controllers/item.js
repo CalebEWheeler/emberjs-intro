3d1116ab07b228f309189f52fd41eb96869995b8
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-03-19 11:06:09 -0500

    feat: add the black version of the beats by Dre product image to ../assets/images. create a getter for 'productImage()' in the item controller to return the image path depending on the value of 'color' and pass in 'productImage' as a property to the 'ProductImage' component to be made accessible in the 'image' child component as the value of the img src. This commit will allow the image to change on click depending on the selected color value by the user.

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -6,6 +6,10 @@ import { action } from '@ember/object';
 export default class ItemController extends Controller {
   @tracked color = 'red';
 
+  get productImage() {
+    return `../assets/images/beats-solo-${this.color}.png`
+  }
+
   @action
   onChangeColor(newColor) {
     this.color = newColor;
