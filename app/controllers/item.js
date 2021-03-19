ed0cc78449be15062a85fcaedba3a344bd3a5ce8
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-03-19 12:05:11 -0500

    feat: refactor item route to take in the products object from 'data/products.js' and utilize 'find()' to sort through each product by the id value and return the object information as a model to be made accessible to the templates. In 'image' template I utilized Ember.js helper 'each' to replace the hard coded features to now loop through and create each feature as a 'list' element. I also passed the model colors values to the 'details' component and utilized another 'each' helper and the 'concat' helper to refactor the hard coded color options.

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -4,10 +4,10 @@ import { action } from '@ember/object';
 
 
 export default class ItemController extends Controller {
-  @tracked color = 'red';
+  @tracked color = this.model.colors[0].color;
 
   get productImage() {
-    return `/assets/images/beats-solo-${this.color}.png`; 
+    return this.model.colors.find(({ color }) => color === this.color).image; 
   }
 
   @action
