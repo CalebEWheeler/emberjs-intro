18dbb5fad6ac0de52f6135ff7c211b2026a5d54c
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-03-20 18:52:34 -0500

    feat: update our cart controller, template, service and general-container component to have functionality for updating product information in the cart, as well as a visual tracker for the amount of items in the cart.

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,5 +1,6 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class CartController extends Controller {
   //original default values to pass from controller to view
@@ -10,8 +11,8 @@ export default class CartController extends Controller {
 
   //create getters for each model property
   get subtotal() {
-    return this.model.reduce((acc, item) => {
-      return acc + item.price;
+    return this.cart.itemList.reduce((acc, item) => {
+      return acc + item.price * item.count;
     }, 0);
   }
 
@@ -21,5 +22,15 @@ export default class CartController extends Controller {
 
   get total() {
     return this.subtotal + this.tax;
+  }
+
+  @action 
+  updateItemCount(item, event) {
+    const count = event.target.value;
+    if ( count >= 0 ) {
+      item.count = count;
+    } else {
+      item.count = 0;
+    }
   }
 }
