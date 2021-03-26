2d9a2230cdf83dc84c333f3f67f9dc2e724cd24c
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-03-26 11:48:44 -0500

    feat: add gif-components and pass desired gif and rating values to the gif-container/image component. Create a select menu for changing the trending page url.

--- app/routes/trending.js
+++ app/routes/trending.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class TrendingRoute extends Route {
   model() {
-    const getTrendingGifs = fetch(`https://api.giphy.com/v1/gifs/trending?api_key=5bWPQlkq27JXBNM2Z3gyF85EG5XK0iLt&limit=25&rating=pg-13`)
+    const getTrendingGifs = fetch(`https://api.giphy.com/v1/gifs/trending?api_key=5bWPQlkq27JXBNM2Z3gyF85EG5XK0iLt&limit=25&rating=r`)
     .then(result => result.json())
     .then(trendingGifs => {
       console.log(trendingGifs.data)
